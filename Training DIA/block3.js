33fd0956d39856cf2bbc0edbccbe7fdd953c638a
pkostereva <pkostereva@gmail.com>
2019-11-24 18:24:06 +0300

    some tasks

--- Training DIA/block3.js
+++ Training DIA/block3.js
@@ -1,34 +1,115 @@
 // 11. Сделать реверс массива (массив в обратном направлении)
 
 let randomArr = (t) => {
-        let randomArray = [];
-        for (i = 0; i < t; i++) {
-            randomArray.push(Math.round(Math.random() * 100));
-        }
-        return randomArray;
+    let randomArray = [];
+    for (i = 0; i < t; i++) {
+        randomArray.push(Math.round(Math.random() * 100));
     }
-    // console.log(randomArr(4));
+    return randomArray;
+}
 
-let ultraArr = randomArr(4);
-// console.log('my random array' + ultraArr);
-// task11();
 
 function task11(straightArr) {
     console.log(straightArr);
-    // straightArr = randomArray(10);
     reverseArr = [];
-    for (let i = 0; i <= straightArr.length; i++) {
-        reverseArr[i] = straightArr[straightArr.length - i];
+    n = straightArr.length;
+    for (let i = 0; i <= n - 1; i++) {
+        reverseArr[i] = straightArr[(n - 1) - i];
     }
     return reverseArr;
 }
 
-
 console.log(task11(randomArr(4)))
-    // console.log(task11(randomArr(10)));
 
 
 // 12. Посчитать количество нечетных элементов массива
+
+function task12(straightArr) {
+    console.log(straightArr);
+    counter = 0;
+    n = straightArr.length;
+    for (i = 1; i <= n - 1; i++) {
+        if (i % 2 == 0) {
+            counter += 1;
+        }
+    }
+    return counter;
+}
+console.log(task12(randomArr(5)));
+
 // 13. Поменять местами первую и вторую половину массива, например, для массива 1 2 3 4, результат 3 4 1 2
+
+function task13(straightArr) {
+    console.log(straightArr);
+    let part = straightArr.length / 2;
+    let result = [];
+
+    for (let i = part; i < straightArr.length; i++) {
+        result.push(straightArr[i])
+    }
+
+    for (let i = 0; i < part; i++) {
+        result.push(straightArr[i])
+    }
+    return result;
+}
+
+console.log(task13(randomArr(6)));
+
 // 14. Отсортировать массив пузырьком (Bubble) 
+
+function bubbleSortTask14(arr) {
+    console.log(arr);
+    for (let j = arr.length - 1; j > 0; j--) {
+        for (let i = 0; i < j; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSortTask14(randomArr(10)));
+
 // 15. Отсортировать массив ещё двумя методами (выбором (Select), вставками (Insert))
+
+// Select sort
+
+function selectSortTask15(arr) {
+    console.log(arr);
+    for (let i = 0, l = arr.length, k = l - 1; i < k; i++) {
+        let indexMin = i;
+        for (let j = i + 1; j < l; j++) {
+            if (arr[indexMin] > arr[j]) {
+                indexMin = j;
+            }
+        }
+        if (indexMin !== i) {
+            [arr[i], arr[indexMin]] = [arr[indexMin], arr[i]];
+        }
+    }
+    return arr;
+};
+
+console.log(selectSortTask15(randomArr(10)));
+
+// Insert sort 
+
+function insertionSortTask15(arr) {
+    console.log(arr);
+    for (let i = 1, l = arr.length; i < l; i++) {
+        let current = arr[i];
+        let j = i;
+        while (j > 0 && arr[j - 1] > current) {
+            arr[j] = arr[j - 1];
+            j--;
+        }
+        arr[j] = current;
+    }
+    return arr;
+};
+
+console.log(insertionSortTask15(randomArr(10)));
